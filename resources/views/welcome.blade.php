f82dee733d93b3fa77de592f19f9dad855535733
stecala <ste.cala123@gmail.com>
2022-09-13 14:53:42 +0200

    feat: :sparkles: working on blades

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layout.main')
 
-        <title>Laravel</title>
+@section('title' , 'Home')
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
-
-        <!-- Styles -->
-        <link href="{{asset('css/app.css')}}" rel="stylesheet">
-    </head>
-    <body>
-
-    </body>
-</html>
+@section('main_content')
+	<h1>ciao</h1>
+@endsection
