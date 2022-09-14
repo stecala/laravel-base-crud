beedc30ee22901304f585a0f28154a0f21eb5b3a
stecala <ste.cala123@gmail.com>
2022-09-14 13:09:46 +0200

    style: :art: text center

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,7 +5,7 @@
 @section('main_content')
     <div class="container-lg comic-container">
         <div class="row">
-            <div class="coll-12 mb-5">
+            <div class="coll-12 mb-5 text-center">
                 <h3>{{ $comic->title }}</h3>
             </div>
             <div class="col-3">
